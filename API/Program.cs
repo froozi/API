7cb5712c94200b494f3bdcafa829fe8454ca3579
эльдар хайрутдинов <eldar.khairutdinov@yandex.ru>
2021-06-14 22:54:09 +0300

    Добавьте файлы проекта.

---
+++ API/Program.cs
@@ -0,0 +1,40 @@
+﻿// OpenWeatherMap REST API example
+// DKY 2021
+
+using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ApiKey = "abe6e881d725e775b19a763603537862";
+            //var City = "Kazan";
+            var url = $"https://apidata.mos.ru/v1/datasets/2254/rows?api_key={ApiKey}";
+
+            var request = WebRequest.Create(url);
+
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode != HttpStatusCode.OK)
+            {
+                Console.WriteLine(httpStatusCode);
+                return;
+            }
+
+            using (var streamReader = new StreamReader(response.GetResponseStream()))
+            {
+                string result = streamReader.ReadToEnd();
+                Console.WriteLine(result);
+                var weatherForecast = JsonConvert.DeserializeObject<Texno>(result);
+            }
+
+        }
+    }
+}
+
---
+++ API/Texno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API
+{
+    class Texno
+    {
+        public class GeoData
+        {
+            public List<double> coordinates { get; set; }
+            public string type { get; set; }
+        }
+
+        public class Cells
+        {
+            public string Name { get; set; }
+            public int global_id { get; set; }
+            public string AdmArea { get; set; }
+            public string District { get; set; }
+            public string Address { get; set; }
+            public List<object> PublicPhone { get; set; }
+            public string WebSite { get; set; }
+            public List<object> Specialty { get; set; }
+            public string Infrastructure { get; set; }
+            public GeoData geoData { get; set; }
+        }
+
+        public class Root
+        {
+            public int global_id { get; set; }
+            public int Number { get; set; }
+            public Cells Cells { get; set; }
+        }
+    }
+}
