547e4897199c9219c206aa720c2449202b13b704
Эльдар Хайрутдинов <eldar.khairutdinov@yandex.ru>
2021-06-21 12:13:54 +0300

    add TelegramBot

--- API/Program.cs
+++ API/Program.cs
@@ -1,40 +1,59 @@
-﻿// OpenWeatherMap REST API example
-// DKY 2021
-
-using System;
+﻿using System;
 using System.IO;
-using System.Net;
+using Telegram.Bot;
 using Newtonsoft.Json;
+using System.Net;
 
-namespace API
+namespace TelegramBot
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            TelegramBotClient bot = new TelegramBotClient("1895394169:AAEQSTodVZ9p3IsvCyrL38hRXAExuNF-EVM");
+
+            bot.OnMessage += (s, arg) =>
+            {
+                string a = "Ошибка";
+                try
+                {
+                    a = API();
+                }
+                catch { }
+                Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                bot.SendTextMessageAsync(arg.Message.Chat.Id, $"You say: {a}");
+
+            };
+
+            bot.StartReceiving();
+
+            Console.ReadKey();
+        }
+
+        static string API()
         {
             var ApiKey = "abe6e881d725e775b19a763603537862";
             //var City = "Kazan";
             var url = $"https://apidata.mos.ru/v1/datasets/2254/rows?api_key={ApiKey}";
 
             var request = WebRequest.Create(url);
-
             var response = request.GetResponse();
             var httpStatusCode = (response as HttpWebResponse).StatusCode;
 
             if (httpStatusCode != HttpStatusCode.OK)
             {
                 Console.WriteLine(httpStatusCode);
-                return;
+                return "";
             }
 
             using (var streamReader = new StreamReader(response.GetResponseStream()))
             {
                 string result = streamReader.ReadToEnd();
                 Console.WriteLine(result);
-                var weatherForecast = JsonConvert.DeserializeObject<Texno>(result);
+                return JsonConvert.DeserializeObject<Texno>(result).ToString();
             }
 
         }
     }
-}
 
+}
--- API/Texno.cs
+++ API/Texno.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace API
+namespace TelegramBot
 {
     class Texno
     {
